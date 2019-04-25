d738628e0215064b1606ec189ce3072cf1a243b9
Aaron <kongyajie0704@gmail.com>
2019-04-25 13:32:51 +0800

    add error handle

--- init/koa.js
+++ init/koa.js
@@ -6,6 +6,7 @@ const app = new Koa();
 app.use(cors()); // 处理跨域
 app.use(bodyParser()); // 解析body
 
+
 // 返回格式统一处理
 const response_formatter = require('../util/res');
 app.use(response_formatter);
@@ -13,6 +14,15 @@ app.use(response_formatter);
 const config = require('../conf');
 const router = require('../routes/index');
 app.use(router.routes(), router.allowedMethods());
+
+// 全局error处理
+app.on('error', function(err) {
+  if (process.env.NODE_ENV != 'test') {
+    console.log('sent error %s to the cloud', err.message);
+    console.log(err);
+  }
+});
+
 
 var port = config.port;
 app.listen(port);
--- util/res.js
+++ util/res.js
@@ -3,20 +3,35 @@
  */
 var response_formatter = async (ctx, next) => {
   //先去执行路由
-  await next();
+  try {
+    await next();
+    //如果有返回数据，将返回数据添加到data中
+    if (ctx.body) {
+        ctx.body = {
+            code: 1,
+            body: ctx.body,
+            meta: {}
+        }
+    } else {
+        ctx.body = {
+            code: 0,
+            msg: 'fail'
+        }
+    }
+  } catch (err){
+    // some errors will have .status
+    // however this is not a guarantee
+    ctx.body = {
+        statu: err.status || 500,
+        code: 0,
+        msg: err.message
+    }
 
-  //如果有返回数据，将返回数据添加到data中
-  if (ctx.body) {
-      ctx.body = {
-          code: 1,
-          body: ctx.body,
-          meta: {}
-      }
-  } else {
-      ctx.body = {
-          code: 0,
-          msg: 'fail'
-      }
+    // since we handled this manually we'll
+    // want to delegate to the regular app
+    // level error handling as well so that
+    // centralized still functions correctly.
+    ctx.app.emit('error', err, ctx);
   }
 }
 
